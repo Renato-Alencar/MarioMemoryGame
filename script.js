fd5674dc22e604edb2dd33d14dfbd2d907e46bb6
Renato_Alencar <renato.augustoalencar@gmail.com>
2020-10-01 17:19:07 -0300

    Parte6: Finaliza função lockBoard impedindo que o jogador clique em mais de duas cartas e cria a função resetBoard para retirar o histórico de salvamento das cartas na rodada anterior.

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let lockBoard = false;
 //Lógica do game
 function flipCard() {
     if(lockBoard) return;
-    
+    if(this === firstCard) return;
+
     this.classList.add('flip');
     if(!hasFlippedCard) {
         hasFlippedCard = true;
@@ -30,13 +31,24 @@ function checkForMath() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    
+    resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     },1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card) => {
